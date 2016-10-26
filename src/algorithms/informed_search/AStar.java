306012660dd73aa312031bb54ef4ab0a53278541
mahdi aghajani <mma137421@gmail.com>
2016-10-26 21:25:39 +0330

    Implements A*

--- src/algorithms/informed_search/AStar.java
+++ src/algorithms/informed_search/AStar.java
@@ -1,13 +1,70 @@
 package algorithms.informed_search;
 
 import algorithms.Algorithm;
+import problems.Node;
+import problems.Problem;
+
+import java.util.ArrayList;
 
 /**
  * Created by mma on 10/26/16.
  */
 public class AStar extends Algorithm {
     @Override
-    public void apply() {
+    public Node apply(Problem problem, boolean isGraphy) {
+        if (isGraphy) {
+            f = new ArrayList<>();
+            e = new ArrayList<>();
 
+            while (!f.isEmpty()) {
+                Node node = getMinimumCost(problem);
+                if (problem.isGoal(node)) return node;
+
+                for (Node child : problem.nextState(node)) {
+                    if (!e.contains(child) && !f.contains(child) && !child.equals(node)) {
+                        child.setAccessibilityCost(node.getAccessibilityCost() + problem.getCost(node, child));
+                        f.add(child);
+                    }
+                    //updating node
+                    if (f.contains(child)) {
+                        int newCost = node.getAccessibilityCost() + problem.getCost(node, child) ;
+                        if( newCost < f.get(f.indexOf(child)).getAccessibilityCost())
+                            f.get(f.indexOf(child)).setAccessibilityCost(newCost);
+                    }
+                }
+                e.add(node);
+            }
+        } else {
+            f = new ArrayList<>();
+
+            while (!f.isEmpty()) {
+                Node node = getMinimumCost(problem);
+                if (problem.isGoal(node)) return node;
+
+                for (Node child : problem.nextState(node)) {
+                    if ( !f.contains(child) && !child.equals(node)) {
+                        child.setAccessibilityCost(node.getAccessibilityCost() + problem.getCost(node, child));
+                        f.add(child);
+                    }
+                    //updating node
+                    if (f.contains(child)) {
+                        int newCost = node.getAccessibilityCost() + problem.getCost(node, child) ;
+                        if( newCost < f.get(f.indexOf(child)).getAccessibilityCost())
+                            f.get(f.indexOf(child)).setAccessibilityCost(newCost);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private Node getMinimumCost(Problem problem) {
+        Node min = new Node();
+        min.setAccessibilityCost(Integer.MAX_VALUE);
+        for (Node node : f) {
+            if (problem.evaluateFunction(node) < problem.evaluateFunction(min))
+                min = node;
+        }
+        return min;
     }
 }
