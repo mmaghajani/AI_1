7170e90f7a2bb35795c3baea36490176ee4ecf79
mahdi aghajani <mma137421@gmail.com>
2016-10-26 17:07:15 +0330

    implements BFS

--- src/algorithms/uninformed_search/BFS.java
+++ src/algorithms/uninformed_search/BFS.java
@@ -1,14 +1,47 @@
 package algorithms.uninformed_search;
 
 import algorithms.Algorithm;
+import algorithms.Node;
 import problems.Problem;
+
+import java.util.ArrayList;
 
 /**
  * Created by mma on 10/26/16.
  */
 public class BFS extends Algorithm {
     @Override
-    public void apply(Problem problem , boolean isGraphy) {
+    public Node apply(Problem problem, boolean isGraphy) {
+        if (isGraphy) {
+            f = new ArrayList<>();
+            e = new ArrayList<>();
+            f.add(problem.getInitialState());
+            //Exception forever initial and goal is the same
+            if (problem.isGoal(f.get(0))) return f.get(0);
 
+            while (!f.isEmpty()) {
+                Node node = f.remove(0);
+                for (Node child : problem.nextState(node)) {
+                    if (problem.isGoal(child)) return child;
+                    if (!e.contains(child) && !f.contains(child) && !child.equals(node))
+                        f.add(child);
+                }
+                e.add(node);
+            }
+        } else {
+            f = new ArrayList<>();
+            f.add(problem.getInitialState());
+            //Exception forever initial and goal is the same
+            if (problem.isGoal(f.get(0))) return f.get(0);
+
+            while (!f.isEmpty()) {
+                Node node = f.remove(0);
+                for (Node child : problem.nextState(node)) {
+                    if (problem.isGoal(child)) return child;
+                    f.add(child);
+                }
+            }
+        }
+        return null;
     }
 }
