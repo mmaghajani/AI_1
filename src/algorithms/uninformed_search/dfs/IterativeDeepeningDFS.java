1a85f00e436ee9e3fa4775f39824e2d4605db1df
mahdi aghajani <mma137421@gmail.com>
2016-10-26 19:47:32 +0330

    Implements iterative deepening DFS

--- src/algorithms/uninformed_search/dfs/IterativeDeepeningDFS.java
+++ src/algorithms/uninformed_search/dfs/IterativeDeepeningDFS.java
@@ -3,12 +3,64 @@ package algorithms.uninformed_search.dfs;
 import problems.Node;
 import problems.Problem;
 
+import java.util.ArrayList;
+
 /**
  * Created by mma on 10/26/16.
  */
 public class IterativeDeepeningDFS extends DFS {
+    private int limit = Integer.MAX_VALUE;
+
     @Override
     public Node apply(Problem problem, boolean isGraphy) {
-        return null;
+        if (isGraphy) {
+            limit = 0;
+            while (true) {
+                f = new ArrayList<>();
+                e = new ArrayList<>();
+                f.add(problem.getInitialState());
+
+                //Exception forever initial and goal is the same
+                if (problem.isGoal(f.get(0))) return f.get(0);
+
+                while (!f.isEmpty()) {
+                    Node node = f.remove(0);
+                    if (node.getDepth() < limit) {
+                        for (Node child : problem.nextState(node)) {
+                            if (problem.isGoal(child)) return child;
+                            if (!e.contains(child) && !f.contains(child) && !child.equals(node)) {
+                                child.setDepth(node.getDepth() + 1);
+                                f.add(0, child);
+                            }
+                        }
+                        e.add(node);
+                    }
+                }
+                limit++;
+            }
+        } else {
+            limit = 0;
+            while (true) {
+                f = new ArrayList<>();
+                f.add(problem.getInitialState());
+
+                //Exception forever initial and goal is the same
+                if (problem.isGoal(f.get(0))) return f.get(0);
+
+                while (!f.isEmpty()) {
+                    Node node = f.remove(0);
+                    if (node.getDepth() < limit) {
+                        for (Node child : problem.nextState(node)) {
+                            if (problem.isGoal(child)) return child;
+                            if (!child.equals(node)) {
+                                child.setDepth(node.getDepth() + 1);
+                                f.add(0, child);
+                            }
+                        }
+                    }
+                }
+                limit++ ;
+            }
+        }
     }
 }
