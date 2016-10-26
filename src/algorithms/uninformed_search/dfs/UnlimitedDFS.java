457a49ee2b092f831917395d1c0001038cffd6bd
mahdi aghajani <mma137421@gmail.com>
2016-10-26 19:20:31 +0330

    Implements unlimited DFS search

--- src/algorithms/uninformed_search/dfs/UnlimitedDFS.java
+++ src/algorithms/uninformed_search/dfs/UnlimitedDFS.java
@@ -3,12 +3,47 @@ package algorithms.uninformed_search.dfs;
 import problems.Node;
 import problems.Problem;
 
+import java.util.ArrayList;
+
 /**
  * Created by mma on 10/26/16.
  */
-public class UnlimitedDFS extends DFS{
+public class UnlimitedDFS extends DFS {
     @Override
-    public Node apply(Problem problem , boolean isGraphy) {
+    public Node apply(Problem problem, boolean isGraphy) {
+        if (isGraphy) {
+            f = new ArrayList<>();
+            e = new ArrayList<>();
+            f.add(problem.getInitialState());
+
+            //Exception forever initial and goal is the same
+            if (problem.isGoal(f.get(0))) return f.get(0);
+
+            while (!f.isEmpty()) {
+                Node node = f.remove(0);
+                for (Node child : problem.nextState(node)) {
+                    if (problem.isGoal(child)) return child;
+                    if (!e.contains(child) && !f.contains(child) && !child.equals(node))
+                        f.add(0, child);
+                }
+                e.add(node);
+            }
+        } else {
+            f = new ArrayList<>();
+            f.add(problem.getInitialState());
+
+            //Exception forever initial and goal is the same
+            if (problem.isGoal(f.get(0))) return f.get(0);
+
+            while (!f.isEmpty()) {
+                Node node = f.remove(0);
+                for (Node child : problem.nextState(node)) {
+                    if (problem.isGoal(child)) return child;
+                    if (!child.equals(node))
+                        f.add(0, child);
+                }
+            }
+        }
         return null;
     }
 }
