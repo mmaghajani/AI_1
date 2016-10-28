bbe6657c1174e4211fce94f7830afca35845d0d1
mahdi aghajani <mma137421@gmail.com>
2016-10-28 20:25:00 +0330

    Implements queens problem.to be continued...

--- src/problems/Queens.java
+++ src/problems/Queens.java
@@ -1,6 +1,7 @@
 package problems;
 
 import data_structures.Node;
+import data_structures.QueensNode;
 
 import java.util.ArrayList;
 
@@ -10,22 +11,66 @@ import java.util.ArrayList;
 public class Queens extends Problem {
     @Override
     public Node getInitialState() {
-        return null;
+        int[] state = new int[8];
+        for (int i = 0; i < 8; i++)
+            state[i] = 0;
+        QueensNode node = new QueensNode(state);
+        return node;
     }
 
     @Override
     public ArrayList<Node> nextState(Node node) {
-        return null;
+        ArrayList<Node> next = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            int[] state1 = ((QueensNode) node).getState();
+            if (state1[i] > 0) {
+                state1[i]--;
+                QueensNode nextState = new QueensNode(state1);
+                next.add(nextState);
+            }
+            int[] state2 = ((QueensNode) node).getState();
+            if (state2[i] < 7) {
+                state2[i]--;
+                QueensNode nextState = new QueensNode(state2);
+                next.add(nextState);
+            }
+        }
+        return next;
     }
 
     @Override
     public boolean isGoal(Node node) {
-        return false;
+        return isGaurd(node);
+    }
+
+    private boolean isGaurd(Node node) {
+        int[] state = ((QueensNode) node).getState();
+        int[] mark = new int[8];
+        for (int i = 0; i < 8; i++)
+            mark[i] = 0;
+        for (int i = 0; i < 8; i++) {
+            if (mark[state[i]] != 0)
+                return false;
+            else
+                mark[state[i]] = 1;
+        }
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 8; j++) {
+                if (state[j] == state[i] - (j - i) || state[j] == state[i] + (j - i))
+                    return false;
+            }
+            for (int j = i - 1; j >= 0; j--) {
+                if (state[j] == state[i] - (i - j) || state[j] == state[i] + (i - j))
+                    return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public int getCost(Node src, Node dest) {
-        return 0;
+        return 1;
     }
 
     @Override
