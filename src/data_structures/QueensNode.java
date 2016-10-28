89056c5575070aa8295ca0d58652d5514a0d9d1d
mahdi aghajani <mma137421@gmail.com>
2016-10-28 19:46:56 +0330

    Implements queens node

--- src/data_structures/QueensNode.java
+++ src/data_structures/QueensNode.java
@@ -4,4 +4,17 @@ package data_structures;
  * Created by mma on 10/26/16.
  */
 public class QueensNode extends Node {
+    private int[] state = new int[8];
+
+    public QueensNode(int[] state) {
+        this.state = state;
+    }
+
+    public int[] getState() {
+        return state;
+    }
+
+    public void setState(int[] state) {
+        this.state = state;
+    }
 }
